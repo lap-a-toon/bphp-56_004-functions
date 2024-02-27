7853c14df6d5e55d3fe6e75d9131769d6dd0b36b
Xander Lexx <lap-a-toon@yandex.ru>
2024-02-27 17:46:05 +0500

    exercise-03

--- basket.php
+++ basket.php
@@ -8,12 +8,16 @@ const OPERATION_EXIT = 0;
 const OPERATION_ADD = 1;
 const OPERATION_DELETE = 2;
 const OPERATION_PRINT = 3;
+const OPERATION_RENAME = 4;
+const OPERATION_RECOUNT = 5;
 
 $operations = [
     OPERATION_EXIT => OPERATION_EXIT . '. Завершить программу.',
     OPERATION_ADD => OPERATION_ADD . '. Добавить товар в список покупок.',
     OPERATION_DELETE => OPERATION_DELETE . '. Удалить товар из списка покупок.',
     OPERATION_PRINT => OPERATION_PRINT . '. Отобразить список покупок.',
+    OPERATION_RENAME => OPERATION_RENAME . '. Переименовать позицию в заказе.',
+    OPERATION_RECOUNT => OPERATION_RECOUNT . '. Изменить количество товара в заказе.',
 ];
 
 $items = [];
@@ -22,7 +26,7 @@ do {
     cls();
 
     do {
-        showCart($items);
+        operationPrint($items);
 
         $label = 'Выберите операцию для выполнения: ' . PHP_EOL;
         // Проверить, есть ли товары в списке? Если нет, то не отображать пункт про удаление товаров
@@ -43,24 +47,23 @@ do {
 
     switch ($operationNumber) {
         case OPERATION_ADD:
-            $itemToAdd = doInput('Введение название товара для добавления в список:',true);
-            if($itemToAdd!=='') // Перед добавлением проверяем не пустая ли строка, если пустая - пропускаем шаг
-                $items[] = $itemToAdd;
+            operationAdd($items);
             break;
 
         case OPERATION_DELETE:
             // Проверить, есть ли товары в списке? Если нет, то сказать об этом и попросить ввести другую операцию
-            echo 'Текущий список покупок:' . PHP_EOL;
-            showCart($items);
-
-            $itemName = doInput('Введение название товара для удаления из списка:',true);
-            deleteItem($itemName,$items); // Проверка на наличие товара производится внутри функции
-
+            operationDelete($items);
             break;
 
         case OPERATION_PRINT:
-            showCart($items,$counter=true);
-            doInput('Нажмите enter для продолжения...');
+            operationPrint($items,true);
+            break;
+        
+        case OPERATION_RENAME:
+            operationRename($items);
+            break;
+        case OPERATION_RECOUNT:
+            operationRecount($items);
             break;
     }
 
@@ -69,44 +72,149 @@ do {
 
 echo 'Программа завершена' . PHP_EOL;
 
-
 /**
- * deleteItem
- * Если в массиве ничего нет, то сообщаем об этом
- *
- * @param  string $item Элемент, который будем удалять
- * @param  array $items Массив элементов, из которого будем удалять
+ * findItemByName
+ * Поиск товара по массиву
+ * Если указать $itemName, то будет искать по этому параметру, в противном случае запросит ввод
+ * 
+ * @param  mixed $items массив товаров
+ * @param  mixed $itemName (необязательно) Имя искомого товара
  * @return bool
  */
-function deleteItem(string $item,array &$items):void{
-    if (in_array($item, $items, true) !== false) {
-        while (($key = array_search($item, $items, true)) !== false) {
-            unset($items[$key]);
-        }
+function findItemByName(array $items,string $itemName=''):bool|int {
+    if($itemName===''){
+        echo 'Текущий список покупок:' . PHP_EOL;
+        operationPrint($items);
+        $itemName = inputItemName('Введение название товара для редактирования из списка:');
+    }
+    return array_search($itemName, array_column($items, 'name'));
+}
+
+/**
+ * operationAdd
+ * добавление товара
+ * запрашивает ввод и проверяет на наличие товара в массиве
+ * если такое название уже есть перейдёт к изменению его количества
+ *
+ * @param  mixed $items массив товаров
+ * @return void
+ */
+function operationAdd(array &$items):void{
+    do{
+        $itemToAdd = doInput('Введение название товара для добавления в список:',true);
+        if($itemToAdd === '')
+            echo "Укажите название товара" . PHP_EOL;
+    }while($itemToAdd === '');
+
+    $searchResult=findItemByName($items,$itemToAdd);
+    if($searchResult !== false){
+        echo "Такой товар уже есть в корзине, его количество будет изменено на новое" . PHP_EOL;
+    }else{
+        $items[] = [
+            'name'      => $itemToAdd,
+            'quantity'  => 0,
+        ];
+    }
+    operationRecount($items,$itemToAdd);
+}
+
+/**
+ * operationDelete
+ * Удаление товара
+ * запрашивает имя товара, если таковое есть - удаляет элемент массива со сдвигом
+ *
+ * @param  mixed $items массив товаров
+ * @return void
+ */
+function operationDelete(array &$items):void{
+    $searchResult=findItemByName($items);
+    if($searchResult !== false){
+        array_splice($items,$searchResult,1);
     }else{
         doInput('Указанный товар не найден, нажите Enter и попробуйте другую операцию...');
     }
 }
 
 /**
- * showCart
- * Выводим сисок покупок
+ * operationRename
+ * Переимнование товара в корзине
+ * Если при переименовании находится товар с таким же названием, предлагаем выбрать другое имя
  *
- * @param  array $items Список покупок
- * @param bool $counter Выводить ли счетчик товаров
+ * @param  mixed $items массив товаров
  * @return void
  */
-function showCart(array $items,bool $counter = false):void{
+function operationRename(array &$items):void{
+    $searchResult=findItemByName($items);
+    if($searchResult !== false){
+        do{
+            $itemNameNew = inputItemName('Введение НОВОЕ название товара:');
+            if(findItemByName($items,$itemNameNew)!==false)
+                echo "Товар с таким названием уже есть, выберите другое имя".PHP_EOL;
+        }while(findItemByName($items,$itemNameNew)!==false);
+        $items[$searchResult]['name'] = $itemNameNew;
+    }else{
+        doInput('Указанный товар не найден, нажите Enter и попробуйте другую операцию...');
+    }
+}
+
+/**
+ * operationRecount
+ * Меняет количество товара
+ * По-умолчанию запрашивает Имя товара
+ * Если указать товар $item, то запроса не будет
+ *
+ * @param  mixed $items массив товаров
+ * @param  mixed $item (не обязательно) товар, количество которого будем менять. Используется при добавлении товара
+ * @return void
+ */
+function operationRecount(array &$items, string $item=''):void{
+    $searchResult=findItemByName($items,$item);
+    if($searchResult !== false){
+        $itemQuantity = doInput('Введение количество товара:',true,true);
+        $items[$searchResult]['quantity'] = $itemQuantity;
+    }else{
+        doInput("Указанный товар $item не найден, нажите Enter и попробуйте другую операцию...");
+    }
+}
+
+/**
+ * inputItemName
+ * Запрашиваем ввод имени товара
+ * Если ввод будет пустой - запросит повторно
+ *
+ * @param  mixed $label заголовок перез вводом
+ * @return string
+ */
+function inputItemName(string $label):string{
+    do{
+        $name = doInput($label,true);
+    }while($name==='');
+    return $name;
+}
+
+/**
+ * operationPrint
+ * Вывод списка товаров
+ *
+ * @param  mixed $items Массив с покупками
+ * @param  mixed $operPrint (не обязательно) Выводить ли счетчик и ожидание ввода (используется при выводе списка по команде)
+ * @return void
+ */
+function operationPrint(array $items,bool $operPrint = false):void{
     if (count($items)) {
         echo 'Ваш список покупок: ' . PHP_EOL;
-        echo implode("\n", $items) . "\n";
+        foreach($items as $item){
+            echo "$item[name] - $item[quantity] шт." . PHP_EOL;
+        }
     } else {
         echo 'Ваш список покупок пуст.' . PHP_EOL;
     }
-    if($counter){
+    if($operPrint){
         echo 'Всего ' . count($items) . ' позиций. '. PHP_EOL;
+        doInput('Нажмите enter для продолжения...');
     }
-};
+}
+
 
 /**
  * clearScreen
@@ -151,14 +259,20 @@ function prepareCodePage():string{
  * @param  string $label Строка, которую требуется вывести перед запросом ввода
  * @return string
  */
-function doInput(string $label = "",bool $newString=false):string{
+function doInput(string $label = "",bool $newString=false,bool $numeric = false):string|int{
     echo $label . (($newString)?PHP_EOL.'> ':'');
     $input=fgets(STDIN);
     if(function_exists('isWindows') && isWindows()){
-        return trim(mb_convert_encoding($input,'UTF-8',CODE_PAGE));
+        $prepared = trim(mb_convert_encoding($input,'UTF-8',CODE_PAGE));
     }else{
-        return trim($input);
+        $prepared = trim($input);
     }
+    if($numeric && !is_numeric($prepared)){
+        $result = doInput("Укажите число",true,true);
+    }else{
+        $result = ($numeric)?intval($prepared):$prepared;
+    }
+    return $result;
 }
 
 /**
